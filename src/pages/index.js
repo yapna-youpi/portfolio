e783d32c588f3a0b213751ef8e38ea1d17471196
yapna-youpi <yapnayoupi@gmail.com>
2022-07-17 16:08:15 +0100

    beginning of develop my portfolio with teellwindcss

---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+        <h1 className='text-3xl bg-slate-50 font-bold underline'>hello site gatsbyjs is good!</h1>
+    </div>
+  )
+}
+
+export default index
